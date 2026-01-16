5f78334bcff621d95992973fd58f7737fdb5d478
Tigotg <tigo.desenv@gmail.com>
2026-01-16 00:25:55 -0300

    feat: Commit referente as primeira aulas do curso de C# .Net Algumas que são assuntos mais básico não foram feito o commit.

---
+++ EstruturasDeDecisao/EstruturasDeDecisao/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasDeDecisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if(se)... else(senao)...
+            Console.WriteLine("Digite a sua idade: ");
+            UInt32 Idade = Convert.ToUInt32(Console.ReadLine());
+
+            if (Idade >= 18)
+            {
+                Console.WriteLine("Você é Maior de Idade!");
+            }
+            else
+            {
+                Console.WriteLine("Você é Menor de Idade!");
+            }
+
+
+            //if... else if...
+            Console.WriteLine("Digite seu salário:");
+            UInt32 Salario = Convert.ToUInt32(Console.ReadLine());
+            if (Salario < 1000)
+            {
+                Console.WriteLine("Você ganha menos que 1 salário mínimo!");
+            }
+            else if (Salario < 2000)
+            {
+                Console.WriteLine("Você ganha menos que 2 salários mínimo");
+            }
+            else if (Salario < 5000)
+            {
+                Console.WriteLine("Você ganha menos que 5 salários mínimo");
+            }
+            else
+            {
+                Console.WriteLine("Você é rico!");
+            }
+
+
+
+
+            //Switch (Comutador)
+            Console.WriteLine("Pressione uma tecla do seu teclado:");
+            char tecla = Console.ReadKey(true).KeyChar;
+            switch (tecla)
+            {
+                case 'a':
+                    Console.WriteLine("Você Pressionou a");
+                    break;
+                case 'b':
+                    Console.WriteLine("Você Pressionou b");
+                    break;
+                default:
+                    Console.WriteLine("Eu não conheço essa tecla!");
+                    break;
+            }        
+            Console.ReadKey();      
+
+        }
+    }
+}
---
+++ OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresLogicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Var1 = true;
+            bool Var2 = false;
+
+            //Negação representado por: !
+            bool ResultadoNegacao = !Var1;
+            Console.WriteLine("A negação de: " + Var1 + " é " + ResultadoNegacao);
+
+            ResultadoNegacao = !Var2;
+            Console.WriteLine("A negação de: " + Var2 + " é " + ResultadoNegacao);
+            Console.WriteLine(" ");
+
+
+            //Operador E (AND) representado por: &
+            bool ResultadoE = Var1 & Var1;
+            Console.WriteLine("O resultado da operação E entre " + Var1 + " e " + Var1 + " resulta em: " + ResultadoE);
+
+            ResultadoE = Var1 & Var2;
+            Console.WriteLine("O resultado da operação E entre " + Var1 + " e " + Var2 + " resulta em: " + ResultadoE);
+
+            ResultadoE = Var2 & Var2;
+            Console.WriteLine("O resultado da operação E entre " + Var2 + " e " + Var2 + " resulta em: " + ResultadoE);
+            Console.WriteLine(" ");
+
+
+            //Operador OU (OR) representado por: |
+            bool ResultadoOU = Var1 | Var1;
+            Console.WriteLine("O resultado da operação OU entre " + Var1 + " e " + Var1 + " resulta em: " + ResultadoOU);
+
+            ResultadoE = Var1 | Var2;
+            Console.WriteLine("O resultado da operação OU entre " + Var1 + " e " + Var2 + " resulta em: " + ResultadoOU);
+
+            ResultadoE = Var2 | Var2;
+            Console.WriteLine("O resultado da operação OU entre " + Var2 + " e " + Var2 + " resulta em: " + ResultadoOU);
+            Console.WriteLine(" ");
+
+
+            //Operador OU EXCLUSIVO (XOR) representado por: ^
+            bool ResultadoXOR = Var1 ^ Var1;
+            Console.WriteLine("O resultado da operação XOR entre " + Var1 + " e " + Var1 + " resulta em: " + ResultadoXOR);
+
+            ResultadoXOR = Var1 ^ Var2;
+            Console.WriteLine("O resultado da operação XOR entre " + Var1 + " e " + Var2 + " resulta em: " + ResultadoXOR);
+
+            ResultadoXOR = Var2 ^ Var2;
+            Console.WriteLine("O resultado da operação XOR entre " + Var2 + " e " + Var2 + " resulta em: " + ResultadoXOR);
+
+            Console.ReadKey();
+        }
+    }
+}
